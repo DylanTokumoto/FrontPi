d5f90c5d97515c977950e85fe3b1542781a17239
DylanTokumoto <heidy.tokumoto@gmail.com>
2022-11-09 09:50:05 -0300

    responsividade concluida para tablet. mobile 90% concluida 09/11 0950hrs

--- src/components/CardHome/index.jsx
+++ src/components/CardHome/index.jsx
@@ -1,33 +1,32 @@
-import './style.css'
-
 function CardHome({ carro, index }) {
+
     return (
         <>
             <div className="galeria_carro">  {
 
                 carro.map((carro) => (
+
                     <div key={index} className="card_main">
                         <div key={index} className="card_image"> <img src={carro.img} alt="" /> </div>
                         <div className="card_description">
-
                             
                             <div key={index} className="card_title"> <h3>{carro.title}</h3> </div>
                             <div key={index} className="car_description"> 
                             
-                            <h6>{carro.category}</h6>
-                            <p>{carro.description}</p> 
+                            <h6> {carro.category} </h6>
+                            <p> {carro.description} </p> 
                             
                             </div>
 
                             <button className="about">ver mais</button>
+
                         </div>
                     </div>
+                    
                 ))
 
             }
             </div>
-
-
 
         </>
     )
